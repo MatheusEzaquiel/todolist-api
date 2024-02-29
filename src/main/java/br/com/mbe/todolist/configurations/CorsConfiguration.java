994845b888cbfee212dab45e94d6daa388511538
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2024-02-28 21:45:38 -0300

    fix: change Cors Origin to *

--- src/main/java/br/com/mbe/todolist/configurations/CorsConfiguration.java
+++ src/main/java/br/com/mbe/todolist/configurations/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost")
+			.allowedOrigins("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
 			.allowedHeaders("*");
 	}
