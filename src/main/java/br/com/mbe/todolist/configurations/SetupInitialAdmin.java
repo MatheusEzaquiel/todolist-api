94dddffaa6627f03ad154329050af9d89f0bdbdb
matheus <matheusbezaquiel@gmail.com>
2025-11-02 23:22:25 -0300

    Refactor: Update Process to create initial user

--- src/main/java/br/com/mbe/todolist/configurations/SetupInitialAdmin.java
+++ src/main/java/br/com/mbe/todolist/configurations/SetupInitialAdmin.java
@@ -6,6 +6,9 @@ import br.com.mbe.todolist.domain.user.UserRole;
 import br.com.mbe.todolist.domain.user.dto.DetailUserDTO;
 import br.com.mbe.todolist.repository.IUserRepository;
 import br.com.mbe.todolist.service.UserService;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Configuration
 public class SetupInitialAdmin implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(SetupInitialAdmin.class);
     @Autowired
     UserService userService;
 
@@ -21,16 +25,23 @@ public class SetupInitialAdmin implements CommandLineRunner {
     IUserRepository userRepository;
 
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
-        UserDetails user = userRepository.findByUsername("admin");
-        if(user != null) {
-            RegisterDTO admin = new RegisterDTO("admin", "admin@email.com", "123", true, UserRole.ADMIN);
-            DetailUserDTO adminCreated = userService.create(admin);
-            if(adminCreated != null) {
-                System.out.println("Usuário Admin gerado com sucesso!");
-            } else {
-                System.out.println("Erro ao gerar Usuário Admin.");
-            }
-        };
+
+        try {
+            UserDetails user = userRepository.findByUsername("admin");
+            if(user == null) {
+                RegisterDTO admin = new RegisterDTO("admin", "admin@email.com", "123", true, UserRole.ADMIN);
+                DetailUserDTO adminCreated = userService.create(admin);
+                if(adminCreated != null) {
+                    System.out.println("Usuário Admin gerado com sucesso!");
+                } else {
+                    System.out.println("Erro ao gerar Usuário Admin.");
+                }
+            };
+        } catch (Exception ex) {
+            log.warn("Erro ao Gerar Usuário Inicial (Admin)");
+        }
+
     }
 }
